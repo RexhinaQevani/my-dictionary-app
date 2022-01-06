1c9b22af6ef3b3f48fad725e7c2b44e11293a887
RexhinaQevani <reginaqevani@gmail.com>
2022-01-06 17:53:09 +0100

    Styled the page

--- src/SearchEngine.js
+++ src/SearchEngine.js
@@ -4,40 +4,52 @@ import Results from "./Results";
 import "./SearchEngine.css";
 
 export default function SearchEngine() {
-  const [keyword, setKeyword] = useState("");
+  const [keyword, setKeyword] = useState("dictionary");
   const [definitions, setDefinitions] = useState(null);
+  const [loaded, setLoaded] = useState(false);
 
   function handleResponse(response) {
     setDefinitions(response.data[0]);
   }
 
-  function search(event) {
-    event.preventDefault();
-
+  function search() {
     let apiUrl = `https://api.dictionaryapi.dev/api/v2/entries/en_US/${keyword}`;
     axios.get(apiUrl).then(handleResponse);
+  }
+
+  function handleSubmit(event) {
+    event.preventDefault();
+    search();
   }
 
   function handleKeywordChange(event) {
     setKeyword(event.target.value);
   }
 
-  return (
-    <div className="SearchEngine">
-      <form onSubmit={search}>
-        <input
-          type="search"
-          placeholder="Enter word"
-          className="form-control mb-3"
-          onChange={handleKeywordChange}
-        />
-        <input
-          type="submit"
-          value="Search"
-          className="btn btn-secondary mb-3"
-        />
-      </form>
-      <Results results={definitions} />
-    </div>
-  );
+  function load() {
+    setLoaded(true);
+    search();
+  }
+
+  if (loaded) {
+    return (
+      <div className="SearchEngine">
+        <section>
+          <form onSubmit={handleSubmit}>
+            <input
+              type="search"
+              placeholder="Enter word"
+              className="form-control mb-2"
+              onChange={handleKeywordChange}
+            />
+            <input type="submit" value="Search" className="btn" />
+          </form>
+        </section>
+        <Results results={definitions} />
+      </div>
+    );
+  } else {
+    load();
+    return "Loading";
+  }
 }
